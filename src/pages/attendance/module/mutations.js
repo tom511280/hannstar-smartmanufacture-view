ab4e878ab34f1a11340945015c9f8a15d60a3d0f
tomwu <tom511280@gmail.com>
2021-02-03 11:09:17 +0800

    2020-02-03變更 Author: TomWu Content : 1.調整attendanceView業表格間距與日期inputbox高度

--- src/pages/attendance/module/mutations.js
+++ src/pages/attendance/module/mutations.js
@@ -33,7 +33,7 @@ const mutations = {
     state.attendanceData.attendanceList = [];
     state.attendanceData.fields = ['Name','Group','Enter Time','Leave Time','Status','Detail'];
     state.attendanceData.fieldkeys = ['name','tenantName','inTime','outTime','state','detail'];
-    state.attendanceData.fieldsWidth = ["30","40","50","50","25","25"]
+    state.attendanceData.fieldsWidth = ["25","35","45","45","20","20"]
     state.attendanceData.errorCodeList = [];
     state.attendanceData = Object.assign({}, state.attendanceData);
   },
